d9b8febf94efd5c794f9b9ede6d3292ed31a12bf
Harry154git <144781271+Harry154git@users.noreply.github.com>
2023-11-04 19:34:21 +0700

    PRAK302-2310817210010-Harry-Pratama-Yunus.py

--- Modul-3/PRAK302/PRAK302-2310817210010-Harry-Pratama-Yunus.py
+++ Modul-3/PRAK302/PRAK302-2310817210010-Harry-Pratama-Yunus.py
@@ -1,11 +1,11 @@
 a=int(input())
 if a>=80:
     print("A")
-elif a>=70 and a<=79:
+elif a>=70:
     print("B")
-elif a>=60 and a<=69:
+elif a>=60:
     print("C")
-elif a>=50 and a<=59:
+elif a>=50:
     print("D")
 elif a<50:
     print("E")
