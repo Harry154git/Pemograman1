84fb174988f6526dc5d5b4190a2040f407217648
Harry154git <144781271+Harry154git@users.noreply.github.com>
2023-11-04 20:38:03 +0700

    PRAK304-2310817210010-Harry-Pratama-Yunus.c

--- Modul-3/PRAK304/PRAK304-2310817210010-Harry-Pratama-Yunus.c
+++ Modul-3/PRAK304/PRAK304-2310817210010-Harry-Pratama-Yunus.c
@@ -4,19 +4,19 @@ int main(){
     int a;
     scanf("%d",&a);
     if(a>0&&a<10){
-        printf("satuan");
+        printf("Satuan");
     }
     else if(a==0){
-        printf("nol");
+        printf("Nol");
     }
     else if(a>=100){
         printf("Anda Menginput Melebihi Limit Bilangan");
     }
     else if(a>=20){
-        printf("puluhan");
+        printf("Puluhan");
     }
     else if(a>=10){
-        printf("belasan");
+        printf("Belasan");
     }
     return 0;
 }
