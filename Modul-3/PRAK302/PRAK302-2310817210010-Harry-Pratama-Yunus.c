abda74b337fa8a66b1fe93374e657133c649b604
Harry154git <144781271+Harry154git@users.noreply.github.com>
2023-11-04 19:32:43 +0700

    PRAK302-2310817210010-Harry-Pratama-Yunus.c

--- Modul-3/PRAK302/PRAK302-2310817210010-Harry-Pratama-Yunus.c
+++ Modul-3/PRAK302/PRAK302-2310817210010-Harry-Pratama-Yunus.c
@@ -6,13 +6,13 @@ int main(){
     if(a>=80){
         printf("A");
     }
-    else if(a>=70&&a<=79){
+    else if(a>=70){
         printf("B");
     }
-    else if(a>=60&&a<=69){
+    else if(a>=60){
         printf("C");
     }
-    else if(a>=50&&a<=59){
+    else if(a>=50){
         printf("D");
     }
     else if(a<50){
